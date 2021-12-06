28204e12fc60b6711bd3d7f40dc330ff5a474bda
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-12-06 19:47:57 +0100

    trying to get tags to work

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -1,39 +1,52 @@
 window.onload = function() {
-    fetchAllBloggPosts();
+    fetchAllBlogPosts();
 }
 
-async function fetchAllBloggPosts() {
+async function fetchAllBlogPosts() {
     try {
         let response = await fetch('http://localhost:5000/posts');
-        let bloggPosts = await response.json();
+        let blogPosts = await response.json();
 
-        let bloggPostsHTML = '';
-        for(let blogg of bloggPosts) {
-            let bloggDate = new Date(blogg.date);
-            let formatedDate = `${bloggDate.getFullYear()}-${bloggDate.getMonth() + 1}-${bloggDate.getDate()} ${bloggDate.getHours()}:${bloggDate.getMinutes()}`
+        let blogPostsHTML = '';
+        for(let blog of blogPosts) {
+            let blogDate = new Date(blog.date);
+            let formatedDate = `${blogDate.getFullYear()}-${blogDate.getMonth() + 1}-${blogDate.getDate()} ${blogDate.getHours()}:${blogDate.getMinutes()}`
 
-            console.log(blogg['_id'])
-            bloggPostsHTML += `
+            console.log(blog['_id'])
+            blogPostsHTML += `
+                <tr> 
+                    <th> Title </th> <th> Author </th> <th> Date </th> <th> Action </th>
+                </tr>
+
+                <tr> 
+                    <td> ${blog.title} </td> <td> ${blog.author} </td> <td> ${blog.tags} </td> <td> ${formatedDate} </td> 
+                    <td>  
+                        <a href="../admin/update-post.html?id=${blog['_id']}">Update</a> |
+                        <a class="delete-link" data-id="${blog['_id']}" href="#">Delete</a>
+                    </td>
+                </tr>
+            `
+                /* `
                 <li class="list-group-item">
-                    <p>${blogg.content} <br> <span class="date">- ${formatedDate}</span> </p>
+                    <p>${blog.content} <br> <span class="date">- ${formatedDate}</span> </p> 
                     
                     <div>
-                        <a href="../admin/update-post.html?id=${blogg['_id']}">Update</a> |
-                        <a class="delete-link" data-id="${blogg['_id']}" href="#">Delete</a> 
+                        <a href="../admin/update-post.html?id=${blog['_id']}">Update</a> |
+                        <a class="delete-link" data-id="${blog['_id']}" href="#">Delete</a> 
                     </div>
                 </li>
-            `;
+            `; */
         }
 
-        document.getElementById('blogg-list').innerHTML = bloggPostsHTML;
+        document.getElementById('blog-list').innerHTML = blogPostsHTML;
     } catch(error) {
         console.log(error);
     }  
 
-    deleteBloggEvent();
+    deleteBlogEvent();
 }
 
-function deleteBloggEvent() {
+function deleteBlogEvent() {
     let deleteLinks = document.getElementsByClassName('delete-link');
 
     for (let link of deleteLinks) {
