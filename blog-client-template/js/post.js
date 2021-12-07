0cdcea082cfb69947e7188b13c9603ccea48d0e3
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-12-07 17:36:09 +0100

    making the code easier to read

--- blog-client-template/js/post.js
+++ blog-client-template/js/post.js
@@ -1,17 +1,11 @@
 window.onload = function() {
-    
-   
     const queryString = window.location.search;
     let urlParams = new URLSearchParams(queryString)
     let idFromUrl = urlParams.get('id')
-    console.log(urlParams)
-    console.log(idFromUrl)
-
     getPost()
     
     async function getPost() {
         let blog = document.getElementById('blog-list');
-        
         
         try {
             await fetch(`http://localhost:5000/posts/${idFromUrl}`)
@@ -20,12 +14,10 @@ window.onload = function() {
                     let postDate = new Date(post.date)
                     let formatedDate = `${postDate.getFullYear()}-${postDate.getMonth() + 1}-${postDate.getDate()} ${postDate.getHours()}:${postDate.getMinutes()}`
                     blog.innerHTML = `
+
                     <h2> ${post.title}</h2>
-
                     <p><strong>Tags:</strong>${post.tags.join(", ")}</p>
-
                     <p><i><strong>${post.author}</strong></i> - ${formatedDate}</p>
-
                     <p>${post.content}</p>
                     `
                 })
