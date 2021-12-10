061f3c596e113b1d7de1d528240f570888ca488c
JesperHagman <jesper.hagman98@gmail.com>
2021-12-10 12:52:55 +0100

    checking all code

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -14,7 +14,7 @@ function updateBlogEvent() {
         }
 
         try {
-            await fetch('', {       //Blogginläggets id
+            await fetch('', {       //Blogginläggets id  // Satte id=${blog['_id'] så kolla på post.js hur jag gjorde där -Jesper
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json'
