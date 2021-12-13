50b9bb3883939264fa8adb5082347cc982a24fc6
Vitoleander <89918725+Vitoleander@users.noreply.github.com>
2021-12-14 00:33:27 +0100

    update html, css, js code

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -1,30 +1,71 @@
+/* 
+const queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let idFromUrl = urlParams.get('id');
+
+(`http://localhost:5000/posts/${idFromUrl}`, {       //Blogginläggets id  // Satte id=${blog['_id'] så kolla på post.js hur jag gjorde där -Jesper
+*/
+
 window.onload = function () {
-    updateBlogEvent();
+    getBlogPost();
+}
+const queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    let idFromUrl = urlParams.get('id');
+    const url = (`http://localhost:5000/posts/${idFromUrl}`);
+
+const submitBtn = document.getElementById('submit-btn');
+const updatePost = document.getElementById('update-post');
+
+
+//Calling functions
+submitBtn.addEventListener('click', submitUpdate);
+getBlogPost()
+    .catch(error => {
+        console.log(error);
+    });
+postBlogPost();
+
+//Get post
+async function getBlogPost() {
+    const response = await fetch(`http://localhost:5000/posts/${idFromUrl}`);
+    const blogPost = await response.json();
+    document.getElementById('update-post').innerHTML = blogPost;
+    ///Länka ihop med html input
+}; 
+
+//Update post
+async function submitUpdate(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = updatePost;
+    try {
+    const formData = new FormData(form);
+    const responseData = await postFormDataJson({url, formdata});
+    console.log({responseData});
+} catch (error) {
+    console.log(error);
 }
 
-function updateBlogEvent() {
-    let blogEdit = document.getElementById(''); //Bloggpostens id
+//Submit post
+async function postBlogPost({url, formdata}) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
 
-    blogEdit.addEventListener('submit', async function(e) {
-        e.preventDefault();
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: formDataJsonString,
+    };
 
-        let blogData = new FormData(form);
-        blogDataObject = {
-            "content": blogData.get('content')
-        }
-
-        try {
-            await fetch('', {       //Blogginläggets id  // Satte id=${blog['_id'] så kolla på post.js hur jag gjorde där -Jesper
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(blogDataObject),
-            })
-            location.replace('admin/index.html');
-
-        } catch(error) {
-
-        }
-    })
-}
+    const response = await fetch(url, fetchOptions);
+    if (!response.ok) {
+        const errorMessage = await response();
+        console.log(error);
+    }
+    return response.json();
+}};
