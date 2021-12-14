47214fda23415e033f70a9c574857a28deaa9961
Santa Hermez <Santachalabi@hotmail.se>
2021-12-14 13:21:14 +0100

    finnish create utan css

--- blog-client-template/admin/js/create-post.js
+++ blog-client-template/admin/js/create-post.js
@@ -5,17 +5,27 @@ window.onload = function() {
 
 function createPunEvent() {
     let form = document.getElementById('hej');
+    let content = document.getElementById("content");
+    let submit = document.getElementById("submit");
 
     form.addEventListener('submit', async function(e) {
-
         e.preventDefault();
+        let formData = new FormData(form);
+        formDataObject = {
+            "content": formData.get('content'),
+            "author": formData.get('author'),
+            "title": formData.get('title')
+        }
+
+        
         try {
-            await fetch('', {
+            await fetch('http://localhost:5000/posts', {
                 method: 'POST', 
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(formDataObject),
+            
             })
 
             location.replace('index.html');
